780de7f7d066dbc06ab8480b5ee152f22147fed6
dckire <erickcamacho533@gmail.com>
2024-10-24 21:40:23 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+fuction toggleMenu(){
+  var menu =
+    document.getElementByld("menu");
+  menu.classList.toggle("show");
+}
 
+document.addEventListener("DOMContentLoaded",function()
+                          }
+
+                          var menuBtn =
+  document.querySelector(".menu-btn");
+menuBtn.addEventListener("click",
+                         toggleMenu);
+});
